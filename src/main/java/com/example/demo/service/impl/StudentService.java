441cc3144d5512693450454fec4288a844b1d21f
bala-amypo <bala@amypo.in>
2025-12-17 05:34:12 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StudentService.java
+++ src/main/java/com/example/demo/service/impl/StudentService.java
@@ -3,3 +3,13 @@ package com.example.demo.service;
 import java.util.*;
 import org.springframework.stereotype.Service;
 import com.example.demo.entity.StudentEntity;
+
+@Service
+public class StudentService{
+    private final Map<Long, StudentEntity> store = new HashMap<>();
+    private long counter = 1;
+
+    public StudentEntity insertStudent(StudentEntity student){
+        
+    }
+}
