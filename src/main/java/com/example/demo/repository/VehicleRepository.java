e7600bd1c922606d7e10f3111935b2baf626ff90
bala-amypo <bala@amypo.in>
2025-12-18 06:31:52 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/VehicleRepository.java
@@ -0,0 +1,9 @@
+VehicleRepo.java
+
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.Vehicle;
+
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+}
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,15 +1,9 @@
+UserService.java
+
 package com.example.demo.service;
-import java.util.List;
-import java.util.Optional;
-import com.example.demo.entity.Student;
 
-public interface StudentService {
+import com.example.demo.entity.User;
 
-    Student insertStudent(Student st);
-
-    List<Student> getAllStudents();
-
-    Optional<Student> getOneStudent(Long id);
-
-    void deleteStudent(Long id);
+public interface UserService {
+    User registerUser(User user);
 }
